3071ccc6a6d6953520e828a11c5ba6d8238615db
Brian Burrous <brianon007@gmail.com>
2022-03-01 12:25:00 -0800

    asdg

--- app/thermo.py
+++ app/thermo.py
@@ -41,7 +41,7 @@ def linearInterpolate(x1, x2, y1, y2, x):
 
 def getThermoData(filepath, T):
     data = {}
-    with open('./Data/water.json') as json_file:
+    with open('~/app/Data/water.json') as json_file:
         tmp = json.load(json_file)
         data.update(tmp)
     return(getAllProps(data, T))
