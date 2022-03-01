37cf706fc5b96a75d7ffa968b251419e15bf696e
Brian Burrous <brianon007@gmail.com>
2022-03-01 02:36:56 -0800

    Changes

--- app/app.py
+++ app/app.py
@@ -5,8 +5,19 @@ app = Flask(__name__)
  
 @app.route('/')
 def index():
-    foo() 
     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+@app.route('/data', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'POST':
+        material = request.form['material'].lower()
+        Temp = int(request.form['Temp'])
+    else:
+        material = request.args.get('material')
+        Temp = int(request.args.get('Temp'))
+    data = getThermoData(material, Temp)
+    return data 
