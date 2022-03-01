ee46ed8739b393c9b6854b515b6752d373eb9ecc
Brian Burrous <brianon007@gmail.com>
2022-03-01 02:33:33 -0800

    Adding thermo capabilities

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from .test import foo
+from .thermo import getThermoData
  
 app = Flask(__name__)
  
---
+++ app/thermo.py
@@ -0,0 +1,56 @@
+import json
+
+
+
+
+
+def getProp(data, T, prop):
+    Tmin = max(filter(lambda x: x <= T, data["T"]))
+    xMin = data["T"].index(Tmin)
+    Tmax = min(filter(lambda x: x>= T, data["T"]))
+    xMax = data["T"].index(Tmax)
+
+    propMin = data[prop][xMin]
+    propMax = data[prop][xMax]
+    
+    if Tmin != Tmax:
+        return(linearInterpolate(Tmin, Tmax, propMin, propMax, T))
+    else:
+        return(propMin)
+    
+
+
+
+
+def getAllProps(data, T):
+    values = {}
+    for prop in (data["labels"]):
+       values[prop] = getProp(data, T, prop) 
+    return(values)
+
+
+
+
+
+def linearInterpolate(x1, x2, y1, y2, x):
+    slope = (y2-y1)/(x2-x1)
+    return(y1 + slope*x)
+            
+
+
+
+def getThermoData(substance, T):
+    filepath = "Data/" + substance + ".json"
+    data = {}
+    with open(filepath) as json_file:
+        tmp = json.load(json_file)
+        data.update(tmp)
+    return(getAllProps(data, T))
+
+
+
+
+
+
+
+    
