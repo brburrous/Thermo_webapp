c861a0763cabd93a18ffefbf8caf888e740d5f01
Brian Burrous <brianon007@gmail.com>
2022-03-01 02:29:51 -0800

    Added other python file

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, render_template
+from test import foo
  
 app = Flask(__name__)
  
 @app.route('/')
 def index():
+    foo() 
     return render_template('index.html')
 
 if __name__ == '__main__':
